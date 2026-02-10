378e402fef634a561fcfc544de99c104aa5addd8
Nanashi31 <yaelrr302@gmail.com>
2026-02-10 12:11:21 -0600

    Api cambiada

--- seguridad-movil/lib/supabase.ts
+++ seguridad-movil/lib/supabase.ts
@@ -2,11 +2,12 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://vlxfhhmruwafetcxtqti.supabase.co';
-const supabaseAnonKey = 'sb_publishable_rbcmw3T7_laKcoo9LcW1eQ_CQb4Bv48';
+const supabaseUrl = 'https://uwhlbpaabyfoomnlkktt.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3aGxicGFhYnlmb29tbmxra3R0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5NjU3MjAsImV4cCI6MjA4NDU0MTcyMH0.0E6oNSpArkYOsdxiGiSYAWmCyQxSkHWQ8DjXuBcTVZU';
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Supabase URL and Anon Key are required.");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
